e1f3169a54c97dad2c4879838f0511a79a835f6f
maxime vincent <maxime.vincent1@u-psud.fr>
2020-03-07 14:23:18 +0100

    Ajout du lien TP4

--- README.ml
+++ README.ml
@@ -1,4 +1,4 @@
-(*******************************************************************
+ (******************************************************************
  * Travaux Pratiques Introduction à la Programmation Fonctionnelle *
  * UE IPF -> INFO 231 ; Enseignant-Chercheur -> M. Sylvain Conchon *
  * Responsable de TP -> Mme/Mlle Rébecca Zucchini                  *
@@ -7,9 +7,10 @@
  * TP1 -> https://www.lri.fr/~conchon/IPF/TP/2019-2020/1/tp1.html  *
  * TP2 -> https://www.lri.fr/~conchon/IPF/TP/2019-2020/2/tp2.html  *
  * TP3 -> https://www.lri.fr/~conchon/IPF/TP/2019-2020/3/tp3.html  *
+ * TP4 -> https://www.lri.fr/~conchon/IPF/TP/2019-2020/4/tp4.html  *
  *                                                                 *
  * Etudiant -> M. Maxime Vincent (L2 INFO)                         *
- ******************************************************************)
+ ******************************************************************) 
  
 Pour ajouter cette entête à tous les fichiers de code, supprimer
 cette instruction et exécuter :
